ba9c05c1512d896719c371b338270f1dbc00b36a
tpetchel <tpetchel@gmail.com>
2015-08-04 15:05:15 -0400

    just ICMPv4 for simplicity

--- chef-repo/cookbooks/audit/recipes/default.rb
+++ chef-repo/cookbooks/audit/recipes/default.rb
@@ -14,23 +14,20 @@ control_group 'Validate web services' do
 end
 
 control_group 'Validate network configuration and firewalls' do
-  (ICMPv4 ICMPv6).each { |protocol|
-    control "Ensure the firewall blocks public #{protocol} Echo Request messages" do
-      it 'has at least one rule that blocks access' do
-        let(:icmp_types) { { 'ICMPv4' => 8, 'ICMPv6' => 128 } }
-        expect(command(<<-EOH
-          (Get-NetFirewallPortFilter -Protocol #{protocol} |
-          Where-Object { $_.IcmpType -eq #{icmp_types[#{protocol}]} } |
-          Get-NetFirewallRule |
-          Where-Object {
-            ($_.Profile -eq "Public") -and
-            ($_.Direction -eq "Inbound") -and
-            ($_.Enabled -eq "True") -and
-            ($_.Action -eq "Block") } |
-          Measure-Object).Count -gt 0
-          EOH
-        ).stdout).to match(/True/)
-      end
+  control 'Ensure the firewall blocks public ICMPv4 Echo Request messages' do
+    it 'has at least one rule that blocks access' do
+      expect(command(<<-EOH
+        (Get-NetFirewallPortFilter -Protocol ICMPv4 |
+        Where-Object { $_.IcmpType -eq 8 } |
+        Get-NetFirewallRule |
+        Where-Object {
+          ($_.Profile -eq "Public") -and
+          ($_.Direction -eq "Inbound") -and
+          ($_.Enabled -eq "True") -and
+          ($_.Action -eq "Block") } |
+        Measure-Object).Count -gt 0
+        EOH
+      ).stdout).to match(/True/)
     end
-  }
+  end
 end
--- chef-repo/cookbooks/webserver/recipes/default.rb
+++ chef-repo/cookbooks/webserver/recipes/default.rb
@@ -4,32 +4,29 @@
 #
 # Copyright (c) 2015 The Authors, All Rights Reserved.
 # Block ICMPv4 and ICMPv6 Echo Request messages in the public profile.
-icmp_types = { 'ICMPv4' => 8, 'ICMPv6' => 128 }
-%w(ICMPv4 ICMPv6).each { |protocol|
-  powershell_script "Block #{protocol} Echo Request messages" do
-    code <<-EOH
-      Get-NetFirewallPortFilter -Protocol #{protocol} |
-      Get-NetFirewallRule |
-      Where-Object {
-        ($_.Profile -eq "Public") -and
-        ($_.Direction -eq "Inbound") -and
-        ($_.Action -eq "Allow") } |
-      Set-NetFirewallRule -Action Block -IcmpType #{icmp_types[#{protocol}]} -Enabled True
-    EOH
-    guard_interpreter :powershell_script
-    not_if <<-EOH
-      (Get-NetFirewallPortFilter -Protocol #{protocol} |
-      Where-Object { $_.IcmpType -eq #{icmp_types[#{protocol}]} } |
-      Get-NetFirewallRule |
-      Where-Object {
-        ($_.Profile -eq "Public") -and
-        ($_.Direction -eq "Inbound") -and
-        ($_.Enabled -eq "True") -and
-        ($_.Action -eq "Block") } |
-      Measure-Object).Count -gt 0
-    EOH
-  end
-}
+powershell_script 'Block ICMPv4 Echo Request messages' do
+  code <<-EOH
+    Get-NetFirewallPortFilter -Protocol ICMPv4 |
+    Get-NetFirewallRule |
+    Where-Object {
+      ($_.Profile -eq "Public") -and
+      ($_.Direction -eq "Inbound") -and
+      ($_.Action -eq "Allow") } |
+    Set-NetFirewallRule -Action Block -IcmpType 8 -Enabled True
+  EOH
+  guard_interpreter :powershell_script
+  not_if <<-EOH
+    (Get-NetFirewallPortFilter -Protocol ICMPv4 |
+    Where-Object { $_.IcmpType -eq 8 } |
+    Get-NetFirewallRule |
+    Where-Object {
+      ($_.Profile -eq "Public") -and
+      ($_.Direction -eq "Inbound") -and
+      ($_.Enabled -eq "True") -and
+      ($_.Action -eq "Block") } |
+    Measure-Object).Count -gt 0
+  EOH
+end
 
 # Install IIS.
 powershell_script 'Install IIS' do
