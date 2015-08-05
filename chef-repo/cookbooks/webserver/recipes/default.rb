ae135ca08a7e5eea0025876267c49dcac3c24e86
tpetchel <tpetchel@gmail.com>
2015-08-05 14:25:10 -0400

    double to single quotes

--- chef-repo/cookbooks/webserver/recipes/default.rb
+++ chef-repo/cookbooks/webserver/recipes/default.rb
@@ -7,7 +7,7 @@
 powershell_script 'Install IIS' do
   code 'Add-WindowsFeature Web-Server'
   guard_interpreter :powershell_script
-  not_if "(Get-WindowsFeature -Name Web-Server).Installed"
+  not_if '(Get-WindowsFeature -Name Web-Server).Installed'
 end
 
 # Enable and start W3SVC.
